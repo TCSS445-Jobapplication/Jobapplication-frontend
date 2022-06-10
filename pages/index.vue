45d706869a16eeeb1897b953c6157317d8244db6
Vu Nguyen <vhoangvu@uw.edu>
2022-06-10 15:39:38 +0000

    Deploy

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,17 @@
 <template>
   <main class="container-fluid">
     <b-row>
-      <b-col>
+      <b-col cols="3">
         <nuxt-link to="/profile?id=1">
           <b-img thumbnail fluid src="https://picsum.photos/250/250/?image=54" alt="Image 1"></b-img>
         </nuxt-link>
       </b-col>
-      <b-col>
+      <b-col cols="3">
         <nuxt-link to="/profile?id=2">
           <b-img thumbnail fluid src="https://picsum.photos/250/250/?image=58" alt="Image 2"></b-img>
         </nuxt-link>
       </b-col>
-      <b-col>
+      <b-col cols="3">
         <nuxt-link to="/profile?id=3">
           <b-img thumbnail fluid src="https://picsum.photos/250/250/?image=59" alt="Image 3"></b-img>
         </nuxt-link>
